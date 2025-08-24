7fa22fe0c25b0a53bd99aa38698e2f9f19391ac7
mar-lau <marcuslau09@gmail.com>
2025-08-24 15:42:22 -0600

    add report function

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from stats import get_word_count
+from stats import *
+import sys
 
 def get_book_text(filepath):
     with open(filepath) as f:
         text = f.read()
-        get_word_count(text)
+        w_count = get_word_count(text)
+        c_count = get_char_count(text)
+        print_report(filepath, w_count, c_count)
 
 def main():
-    get_book_text("./books/frankenstein.txt")
+    get_book_text("books/frankenstein.txt")
 
 main()
--- stats.py
+++ stats.py
@@ -1,4 +1,26 @@
 def get_word_count(text):
     text_array = text.split()
     num_words = len(text_array)
-    print(f"{num_words} words found in the document")
+    return num_words
+
+def get_char_count(text):
+    text = text.lower()
+    c_dic = {}
+    for c in text:
+        if c in c_dic:
+            c_dic[c] += 1
+        else:
+            c_dic[c] = 1
+    return c_dic
+
+def print_report(filepath, wc, dic):
+    report_sort = sorted(dic.items(), key=lambda x:x[1], reverse=True)
+    print("=============== BOOKBOT ===============")
+    print(f"Analyzing book found at {filepath}")
+    print("------------ Word Count --------------")
+    print(f"Found {wc} total words")
+    print("------ Character Count ------")
+    for report_list in report_sort:
+        if report_list[0].isalpha():
+            print(f"{report_list[0]}: {report_list[1]}")
+    print("================= END =================")
